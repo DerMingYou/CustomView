da5b8b4fb59b7f89891ed90af1a66fa8a2dccec0
尤德铭 <youdm@1jbest.cn>
2019-03-06 16:38:54 +0800

    仿掌阅小说打开方式

---
+++ app/src/main/java/com/ydm/explore/BookOpenModeActivity.java
@@ -0,0 +1,28 @@
+package com.ydm.explore;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.ydm.explore.base.BaseActivity;
+
+/**
+ * Description: 掌阅小说打开方式
+ * Data：2019/3/6-16:32
+ * Author: DerMing_You
+ */
+public class BookOpenModeActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_book_open_mode;
+    }
+
+    @Override
+    public void initView(Bundle savedInstanceState, View contentView) {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/ydm/explore/adapter/BookOpenModeAdapter.java
@@ -0,0 +1,9 @@
+package com.ydm.explore.adapter;
+
+/**
+ * Description:
+ * Data：2019/3/6-16:38
+ * Author: DerMing_You
+ */
+public class BookOpenModeAdapter {
+}
